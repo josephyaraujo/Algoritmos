68df5fe4d0426cf87d6656397497a346a3b9f7fc
josephyaraujo <josephy.araujo@escolar.ifrn.edu.br>
2024-10-22 08:29:06 -0300

    Create mdc.c

---
+++ aula_02/mdc.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int n1, n2, maior, menor, valor;
+    printf("Digite dois números: ");
+    scanf("%d %d", &n1, &n2);
+    
+    if (n1 > n2){
+        maior = n1;
+        menor = n2;
+    }
+    else ^{
+        maior = n2;
+        menor = n1;
+    }
+    while (valor == 0){
+        if (menor == maior-menor){
+            value = 1;
+        }
+        else if (menor > maior - menor){
+            value = 0
+        }
+    }
+
+
+    }
+    return 0;
+}

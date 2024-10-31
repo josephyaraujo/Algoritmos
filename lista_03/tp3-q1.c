365e6be7b7eade44eb15dbdb79a2dd93618ec34a
josephyaraujo <josephy.araujo@escolar.ifrn.edu.br>
2024-10-31 12:18:54 +0000

    primeiro

--- lista_03/tp3-q1.c
+++ lista_03/tp3-q1.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+int main (){
+    int n;
+    printf ("Quantos números terão na sua coleção? ");
+    scanf ("%d", &n);
 
+    int colecao[n];
+
+    printf("Digite os %d números desejados, com espaço entre eles:\n", n);
+    for (int i = 0; i < n; i++){
+        scanf ("%d", &colecao[i]);
+    }
+    printf("A coleção de números na ordem inversa é a seguinte:\n");
+    for (int i = n-1; i >= 0; i--){
+        printf ("%d ", colecao[i]);
+    }
+    printf("\n");
+    return 0;
+}

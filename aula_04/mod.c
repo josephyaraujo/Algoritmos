a7461f3d0e5ff27e6ea28ea0652aa58a26054e54
josephyaraujo <josephy.araujo@escolar.ifrn.edu.br>
2024-11-05 11:22:44 +0000

    primeira

--- aula_04/mod.c
+++ aula_04/mod.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+int mod( int a, int b){
+    int sub = a-b;
+    if (b>sub){
+        return sub;
+    }
+    else{
+        sub= sub-b; 
+        return sub;
+    }
+}
+int main(){    
+    int resultado , a , b;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    resultado = mod(a,b);
+    printf ("%d\n", resultado);
+    
+    return 0;
+}
 

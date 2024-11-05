a9a7766f82b9ad871dff73cb83e53359f510c183
josephyaraujo <josephy.araujo@escolar.ifrn.edu.br>
2024-11-05 12:27:10 +0000

    terceiro

--- aula_04/divrec.c
+++ aula_04/divrec.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
-int mod( int a, int b){
-    int sub = a-b;
-    if (b>sub){
-        return sub;
+int div( int a, int b){
+    int cont = 0;
+    int x = a - b;
+    if (b>x){
+        return cont +=1;
     }
     else{
-        sub= sub-b; 
-        return sub;
+        return div (x,b);
     }
 }
 int main(){    
     int resultado , a , b;
     scanf("%d",&a);
     scanf("%d",&b);
-    resultado = mod(a,b);
+    resultado = div(a,b);
     printf ("%d\n", resultado);
     
     return 0;

02725357f8eee2e3eff817dfc3f7a66ebc2604ff
josephyaraujo <josephy.araujo@escolar.ifrn.edu.br>
2024-11-12 10:33:47 +0000

    teste5

--- aula_04/campoMinado.c
+++ aula_04/campoMinado.c
@@ -7,16 +7,16 @@ int main(){
         {0,-1,0,0,-1},
         {-1,0,-1,0,0}
     };
-    for (int i = 0; i<5; i++){
-        for (int j = 0; j<5; j++){
-            int contador = 0;
+    for (int i = 0; i < 5; i++){
+        for (int j = 0; j < 5; j++){
             if (campo [i][j] == 0){
-                for (int k = i-1; k <i+1; k++){
-                    for (int l = j-1; l< j+1; l++){
+                int contador = 0;
+                for (int k = i-1; k < i+1; k++){
+                    for (int l = j-1; l < j+1; l++){
                         if (k<0 || k>4 || l<0 || l>4){
                             continue;
                         }
-                        if (campo [i][j]== -1){
+                        if (campo [k][l]== -1){
                             contador++;
                         }
                     }
@@ -24,10 +24,10 @@ int main(){
                 campo[i][j] = contador;
             }
         }
-        for (int i = 0; i<5; i++){
-            for (int j = 0; j<5; j++){
-                printf ("%d ", campo [i][j]);
-            }
+    }
+    for (int i = 0; i<5; i++){
+        for (int j = 0; j<5; j++){
+            printf ("%d ", campo [i][j]);
         }
         printf("\n");
     }

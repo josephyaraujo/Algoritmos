156fe14e8258bd4c6365f529820c4f92f9a2b21b
josephyaraujo <josephy.araujo@escolar.ifrn.edu.br>
2024-11-12 02:07:52 +0000

    teste1

--- aula_04/campoMinado.c
+++ aula_04/campoMinado.c
@@ -1,1 +1,35 @@
-
+#include <stdio.h>
+int main(){
+    char campo [5][5]={
+        {"o","x","x","o","o"},
+        {"x","o","o","x","o"},
+        {"o","o","o","x","x"},
+        {"o","x","o","o","x"},
+        {"x","o","x","o","o"}
+    };
+    for (int i = 0; i<5; i++){
+        for (int j = 0; j<5; j++){
+            int contador = 0;
+            if (campo [i][j] == "o"){
+                for (int k = i-1; k <i+1; k++){
+                    for (int l = j-1; l< j+1; l++){
+                        if (k<0 || k>4 || l<0 || l>4){
+                            continue;
+                        }
+                        if (campo [i][j]== "x"){
+                            contador++;
+                        }
+                    }
+                }
+                campo[i][j] = contador;
+            }
+            for (int i = 0; i<5; i++){
+                for (int j = 0; j<5; j++){
+                    printf ("%c ", campo [i][j]);
+                }
+            }
+        }
+        print("\n");
+    }
+    return 0;
+}

122ab2f828788bcc7b79735955fc4427f521fb47
josephyaraujo <josephy.araujo@escolar.ifrn.edu.br>
2024-12-10 12:06:23 +0000

    segunda

--- aula_06/agenda.c
+++ aula_06/agenda.c
@@ -4,51 +4,64 @@
 struct Contato {
     char nome[50];
     char telefone[15];
-};
+}; 
 
-int inserir (){
-    struct Contato* inserirContato (char nome[], char telefone[]){
-        struct Contato* contato = (struct Contato*) malloc(sizeof(struct Contato));
-        strcpy (contato->nome, nome); 
-        strcpy (contato->telefone, telefone)
+void inserir (struct Contato agenda [], int contador){
+    printf("Digite o nome do contato %d: ", contador + 1);
+    scanf("%s", agenda[contador].nome);
 
-        return contato;
+    printf("Digite o telefone do contato %d: ", contador + 1);
+    scanf("%s", agenda[contador].telefone);
+    
     }
-}
-int listar (){
-
-}
-int remover (){
-
-}
-int atualizar (){
-
-}
-int main() {
-    struct Contato agenda[100];
-    int opcoes = 0;
-    int cont = 0;
-
-    printf("Escolha uma das opções: 1 - Inserir, 2 - Listar Contatos, 3 - Remover, 4 - Atualizar");
-    scanf("%d", &opcoes);
-
-    if opcoes == 1:
-        for (int i = 0; i < num_contatos; i++) {
-            printf("Digite o nome do contato %d: ", i + 1);
-            scanf("%s", agenda[i].nome);
-
-            printf("Digite o telefone do contato %d: ", i + 1);
-            scanf("%s", agenda[i].telefone);
-    }
-    if opcoes == 2:
-        printf("Contatos:\n");
-
-        for (int i = 0; i < num_contatos; i++) {
+void listar ( struct Contato agenda[], int contador){
+    printf("Contatos:\n");
+        for (int i = 0; i < contador; i++) {
             printf("Nome: %s\n", agenda[i].nome);
             printf("Telefone: %s\n", agenda[i].telefone);
+        }
     }
-    if opcoes == 3: 
-    if opcoes == 4: 
+void remover (struct Contato agenda[], int opContato){
+    int opcao = opContato -1;
+    for (int i = opcao;  i < (sizeof(agenda)/sizeof(agenda[0])) - 1; i ++){
+        agenda [i] = agenda [i+1];
+    }
+}
+void atualizar (struct Contato agenda[], int opContato){
+    int opcao = opContato -1;
+    struct Contato* contato = &agenda[opcao];
 
+    printf("Digite o nome do contato %d: ", opcao + 1);
+    scanf("%s", agenda[opcao].nome);
+
+    printf("Digite o telefone do contato %d: ", opcao + 1);
+    scanf("%s", agenda[opcao].telefone);
+    
+}
+int main() {
+    int opcoes = 0;
+    int contador = 0;
+    int opContato = 0;
+
+    struct Contato agenda[100];
+    
+    while (opcoes != 9){
+        printf("Escolha uma das opções:\n1 - Inserir,\n2 - Listar Contatos,\n3 - Remover,\n4 - Atualizar,\n9 - Finalizar\n");
+        printf("Digite a opção desejada:");
+        scanf("%d", &opcoes);
+
+        if (opcoes == 1 && cont){
+            }
+        }    
+        if (opcoes == 2){
+            
+        }    
+        if (opcoes == 3){
+
+        } 
+        if (opcoes == 4){
+
+        } 
+    }
     return 0;
 }

3c9aaa328eb7bf630b50b76dfd0184f2fffd7d43
josephyaraujo <josephy.araujo@escolar.ifrn.edu.br>
2025-02-06 15:00:30 +0000

    deu errado

--- aula_07/lista_ligada.c
+++ aula_07/lista_ligada.c
@@ -2,28 +2,36 @@
 #include <stdlib.h>
 
 struct Node* inicio = NULL;
+struct Node* fim = NULL;
 
 struct Node {
     int valor;
+    struct Node* anterior;
     struct Node* proximo;
+    
 }; 
 
 int main() {
     struct Node* novo = (struct Node*)malloc(sizeof(struct Node));
     novo->valor = 10;
-    novo->proximo = inicio; 
-    inicio = novo;
+    novo->anterior = inicio;
+    novo->proximo = fim;
+    inicio = novo; 
+    fim = novo;
     
-    printf ("%d ", *inicio);
+    printf ("%d ", novo->anterior);
+    printf ("%d ", inicio->valor);
     printf ("%d ", novo->proximo);
 
     novo->valor = 5;
-    novo->proximo = inicio; 
-    inicio = novo;
-
-    printf ("%d ", *inicio);
+    novo->anterior = fim;
+    novo->proximo = NULL; 
+    fim->proximo = novo; 
+    fim = novo;    
+    
+    printf ("%d ", novo->anterior);
+    printf ("%d ", inicio->valor);
     printf ("%d ", novo->proximo);
-
 
     return 0;
 }
